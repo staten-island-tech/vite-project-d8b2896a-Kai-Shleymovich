1ca60f1e5652ddf82022e93c282fd23e43576922
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-11-27 13:21:38 -0500

    array of cards done

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,16 @@
-import '../style.css'
 import {clothes} from './clothes'
-
+const domselectors = {
+    flexwrap : document.querySelector(".flex-wrapper")
+}
+function printcards(){
+    clothes.forEach(cloth => {
+    domselectors.flexwrap.insertAdjacentHTML( 
+        "beforeEnd",
+    `<div class="card">
+      <h2>${cloth.name_of_peice}</h2>
+      <img class="card-img" src=${cloth.img} alt="">
+     <p class="desc"> ${cloth.Type_of_clothes}</p>
+     <p>  ${cloth.seller}</p>
+     </div>`)
+})}
+document.addEventListener("DOMContentLoaded", printcards); 
