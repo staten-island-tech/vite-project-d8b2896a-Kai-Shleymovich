8d0d9247f76497b7a3217f6950c1215bc2b7ac30
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2023-11-28 13:18:34 -0500

    filters started

--- project/js/main.js
+++ project/js/main.js
@@ -1,16 +1,37 @@
 import {clothes} from './clothes'
 const domselectors = {
-    flexwrap : document.querySelector(".flex-wrapper")
+    flexwrap : document.querySelector(".flex-wrapper"),
+    skater : document.querySelector("#skater"),
+    hype : document.querySelector('#hype')
 }
+
+let filteredclothes=clothes
+
 function printcards(){
-    clothes.forEach(cloth => {
+    filteredclothes.forEach(cloth => {
     domselectors.flexwrap.insertAdjacentHTML( 
         "beforeEnd",
     `<div class="card">
       <h2>${cloth.name_of_peice}</h2>
       <img class="card-img" src=${cloth.img} alt="">
      <p class="desc"> ${cloth.Type_of_clothes}</p>
-     <p>  ${cloth.seller}</p>
+     <p class='desc'>  ${cloth.seller}</p>
+     <p class='desc'> $${cloth.Price_in_usd} </p>
      </div>`)
 })}
+function filter(x){
+    while(domselectors.flexwrap.lastChild){
+        domselectors.flexwrap.firstChild.remove()
+    }
+    filteredclothes=clothes.filter(cloth => cloth.style.includes(x)===true)
+    printcards();
+}
+domselectors.skater.addEventListener("click", function (event){
+    filter("skater")
+    })
+    domselectors.hype.addEventListener("click", function (event){
+        filter("hypebeast")
+        })
+    
+
 document.addEventListener("DOMContentLoaded", printcards); 
